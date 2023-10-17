d35018499dfa631f28ae81894697a14c3f810e33
MAnandGeovanni <jietaikun@gmail.com>
2023-10-17 21:14:31 +0700

    masih dalam tahap menyimpan dan digunakan data user

--- KamarKita/scripts/script_2.js
+++ KamarKita/scripts/script_2.js
@@ -1,29 +1,56 @@
 // pop up
-function btnLogin(event) {
-  let btnModal = document.getElementById('btnModal');
-  let formLogin = document.getElementById('formLogin');
-  btnModal.classList.remove('hidden');
-  btnModal.classList.add('flex');
-  btnModal.classList.add('oppacity-100');
-  formLogin.classList.add('hidden');
+function btnLogin(e) {
   event.preventDefault();
+  let btnModal = document.getElementById('btnModal');
+  btnModal.classList.add('oppacity-100');
+  btnModal.classList.remove('hidden');
 
-  let email = document.getElementById('email').value;
-  let pass = document.getElementById('pass').value;
+  let newUser = {
+    email: document.getElementById('email').value,
+    password: document.getElementById('password').value,
+  };
+
+  fetch('https://652a0f4f55b137ddc83f45fa.mockapi.io/Datauser', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newUser),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json({ btnModal });
+      } else {
+        return Promise.reject('ada yang tidak sesuai');
+      }
+    })
+    .then((json) => innerHtml(json))
+    .catch((error) => innerHtml('error ' + error));
 }
 
 function btnPageLogin() {
   window.location.replace('index.html');
 }
 
-function btnRegis(event) {
-  let modal = document.getElementById('modal');
-  let formRegis = document.getElementById('formRegis');
-  modal.classList.remove('hidden');
-  modal.classList.add('flex');
-  modal.classList.add('oppacity-100');
-  formRegis.classList.add('hidden');
+function btnRegis(e) {
   event.preventDefault();
+  let modal = document.getElementById('modal');
+  modal.classList.remove('hidden');
+
+  let newUser = {
+    userName: document.getElementById('userName').value,
+    email: document.getElementById('email').value,
+    noHp: document.getElementById('noHp').value,
+    password: document.getElementById('password').value,
+  };
+
+  fetch('https://652a0f4f55b137ddc83f45fa.mockapi.io/Datauser', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newUser),
+  });
 }
 
 function btnPageRegis() {
