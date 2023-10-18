da56bfa3e68233a63f4c1a4d83b76b3e5c83490b
MAnandGeovanni <jietaikun@gmail.com>
2023-10-18 22:50:18 +0700

    perbaikan pop up login

--- KamarKita/scripts/script_2.js
+++ KamarKita/scripts/script_2.js
@@ -1,32 +1,63 @@
-// pop up
-function btnLogin(e) {
-  event.preventDefault();
-  let btnModal = document.getElementById('btnModal');
-  btnModal.classList.add('oppacity-100');
-  btnModal.classList.remove('hidden');
+// login
+// function btnLogin(e) {
+//   event.preventDefault();
+//   let btnModal = document.getElementById('btnModal');
+//   btnModal.classList.add('oppacity-100');
+//   // btnModal.classList.remove('hidden');
 
-  let newUser = {
-    email: document.getElementById('email').value,
-    password: document.getElementById('password').value,
-  };
+//   let newUser = {
+//     email: document.getElementById('email').value,
+//     password: document.getElementById('password').value,
+//   };
 
-  fetch('https://652a0f4f55b137ddc83f45fa.mockapi.io/Datauser', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(newUser),
-  })
-    .then((response) => {
-      if (response.ok) {
-        return response.json({ btnModal });
-      } else {
-        return Promise.reject('ada yang tidak sesuai');
-      }
-    })
-    .then((json) => innerHtml(json))
-    .catch((error) => innerHtml('error ' + error));
-}
+//   fetch('https://652a0f4f55b137ddc83f45fa.mockapi.io/Datauser', {
+//     method: 'GET',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(newUser),
+//   })
+//     .then((response) => {
+//       if (response.ok) {
+//         btnModal.classList.remove('hidden');
+//         return response.json();
+//       } else {
+//         return promise.reject('ada yang tidak sesuai');
+//       }
+//     })
+//     .then((json) => innerWidth(json))
+//     .catch((error) => alert('error ' + error));
+// }
+
+document.addEventListener('DOMContentLoaded', function () {
+  const apiUrl = 'https://652a0f4f55b137ddc83f45fa.mockapi.io/Datauser';
+
+  function newUser(userName, password) {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        const newUser = data.find((newUser) => newUser.userName || email === userName || (email && newUser.password === password));
+        if (newUser) {
+          btnModal.classList.remove('hidden');
+        } else {
+          alert('Username/email atau password salah. Silahkan coba lagi');
+        }
+      })
+      .catch((error) => console.error('Error', error));
+  }
+
+  document.getElementById('loginForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const userName = document.getElementById('userName').value;
+    const password = document.getElementById('password').value;
+
+    if (userName && password) {
+      newUser(userName, password);
+    } else {
+      alert('masukkan username/email dan password');
+    }
+  });
+});
 
 function btnPageLogin() {
   window.location.replace('index.html');
